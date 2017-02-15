602f3c03215549388a97e215392c4ffb2195a1f7
wangfeilong <418593670@qq.com>
2017-02-15 11:46:21 +0800

    联系人界面

--- app/src/main/java/com/im1020/controller/fragment/ContactFragment.java
+++ app/src/main/java/com/im1020/controller/fragment/ContactFragment.java
@@ -1,19 +1,75 @@
 package com.im1020.controller.fragment;
 
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import com.hyphenate.easeui.ui.EaseContactListFragment;
+import com.im1020.R;
+import com.im1020.utils.ShowToast;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * Created by Administrator on 2017/2/15.
  */
 public class ContactFragment extends EaseContactListFragment {
 
-   
+
+    @Bind(R.id.contanct_iv_invite)
+    ImageView contanctIvInvite;
+    @Bind(R.id.ll_new_friends)
+    LinearLayout llNewFriends;
+    @Bind(R.id.ll_groups)
+    LinearLayout llGroups;
+
+    @Override
+    protected void initView() {
+        super.initView();
+
+        //初始化头布局
+        View view = View.inflate(getActivity(), R.layout.fragment_contact_head, null);
+        ButterKnife.bind(this,view);
+        //添加头布局
+        listView.addHeaderView(view);
+        //添加actionbar右侧的加号
+        titleBar.setRightImageResource(R.mipmap.em_add);
+        titleBar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ShowToast.show(getActivity(),"aaa");
+            }
+        });
+    }
+
+    @Override
+    protected void setUpView() {
+        super.setUpView();
+
+    }
+
+    @OnClick({R.id.ll_new_friends, R.id.ll_groups})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.ll_new_friends:
+                ShowToast.show(getActivity(), "aaa");
+                break;
+            case R.id.ll_groups:
+                ShowToast.show(getActivity(), "bbb");
+                break;
+        }
+    }
+
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        ButterKnife.unbind(this);
+    }
+
 }
