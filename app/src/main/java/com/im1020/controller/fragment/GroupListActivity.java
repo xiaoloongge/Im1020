646bbcef25759ae23aff2282628e49fd3ffa52f5
wangfeilong <418593670@qq.com>
2017-02-18 15:22:51 +0800

    群列表点击事件

--- app/src/main/java/com/im1020/controller/fragment/GroupListActivity.java
+++ app/src/main/java/com/im1020/controller/fragment/GroupListActivity.java
@@ -1,13 +1,16 @@
 package com.im1020.controller.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 
 import com.hyphenate.chat.EMClient;
 import com.hyphenate.chat.EMGroup;
+import com.hyphenate.easeui.EaseConstant;
 import com.hyphenate.exceptions.HyphenateException;
 import com.im1020.R;
 import com.im1020.controller.adapter.GroupListAdapter;
@@ -59,6 +62,13 @@ public class GroupListActivity extends AppCompatActivity {
         });
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //刷新界面
+        refresh();
+    }
+
     private void refresh() {
 
         List<EMGroup> allGroups = EMClient.getInstance().groupManager().getAllGroups();
@@ -88,12 +98,37 @@ public class GroupListActivity extends AppCompatActivity {
 
     private void initListener() {
 
+        //创建群组监听
         groupListHead.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 ShowToast.show(GroupListActivity.this,"阿福老师附体了");
             }
         });
+        //item点击事件监听
+
+        lvGrouplist.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                if (position == 0){
+                    return;
+                }
+
+                //跳转到群聊天界面
+                Intent intent = new Intent(GroupListActivity.this,ChatActivity.class);
+
+                EMGroup emGroup = EMClient.getInstance().groupManager().getAllGroups().get(position - 1);
+
+                intent.putExtra(EaseConstant.EXTRA_USER_ID,emGroup.getGroupId());
+
+                intent.putExtra(EaseConstant.EXTRA_CHAT_TYPE,EaseConstant.CHATTYPE_GROUP);
+
+                startActivity(intent);
+
+            }
+        });
+
     }
 
 }
