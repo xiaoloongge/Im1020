e2ccbd4d23d03607737561cd1647ea4a0a254b11
wangfeilong <418593670@qq.com>
2017-02-20 14:35:26 +0800

    群详情的监听

--- app/src/main/java/com/im1020/controller/activity/ChatActivity.java
+++ app/src/main/java/com/im1020/controller/activity/ChatActivity.java
@@ -1,11 +1,17 @@
 package com.im1020.controller.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.FrameLayout;
 
+import com.hyphenate.chat.EMMessage;
+import com.hyphenate.easeui.EaseConstant;
 import com.hyphenate.easeui.ui.EaseChatFragment;
+import com.hyphenate.easeui.widget.chatrow.EaseCustomChatRowProvider;
 import com.im1020.R;
+import com.im1020.utils.ShowToast;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -14,6 +20,7 @@ public class ChatActivity extends AppCompatActivity {
 
     @Bind(R.id.chat_fl)
     FrameLayout chatFl;
+    private EaseChatFragment chatFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,16 +30,74 @@ public class ChatActivity extends AppCompatActivity {
 
 
         initData();
+        initListener();
+    }
+
+    private void initListener() {
+
+        chatFragment.setChatFragmentListener(new EaseChatFragment.EaseChatFragmentHelper() {
+            @Override
+            public void onSetMessageAttributes(EMMessage message) {
+
+            }
+
+            @Override
+            public void onEnterToChatDetails() {
+
+
+                Intent intent = new Intent(ChatActivity.this,ChatDetailsActivity.class);
+                //判断是否是群组
+               /* if (getIntent().getExtras().get(EaseConstant.EXTRA_CHAT_TYPE)
+                        ==EaseConstant.CHATTYPE_GROUP){
+
+                }*/
+                intent.putExtra("userid",getIntent().getExtras()
+                        .getString(EaseConstant.EXTRA_USER_ID));
+
+                startActivity(intent);
+
+            }
+
+            @Override
+            public void onAvatarClick(String username) {
+
+            }
+
+            @Override
+            public void onAvatarLongClick(String username) {
+
+            }
+
+            @Override
+            public boolean onMessageBubbleClick(EMMessage message) {
+                return false;
+            }
+
+            @Override
+            public void onMessageBubbleLongClick(EMMessage message) {
+
+            }
+
+            @Override
+            public boolean onExtendMenuItemClick(int itemId, View view) {
+                return false;
+            }
+
+            @Override
+            public EaseCustomChatRowProvider onSetCustomChatRowProvider() {
+                return null;
+            }
+        });
     }
 
     private void initData() {
 
         //聊天的fragment
-        EaseChatFragment chatFragment = new EaseChatFragment();
+        chatFragment = new EaseChatFragment();
 
         chatFragment.setArguments(getIntent().getExtras());
 
         getSupportFragmentManager().beginTransaction()
-                .replace(R.id.chat_fl,chatFragment).commit();
+                .replace(R.id.chat_fl, chatFragment).commit();
     }
 }
---
+++ app/src/main/java/com/im1020/controller/activity/ChatDetailsActivity.java
@@ -0,0 +1,17 @@
+package com.im1020.controller.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.im1020.R;
+
+public class ChatDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat_details);
+
+
+    }
+}
